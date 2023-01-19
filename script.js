6deeeaae9a4b206c2190258aee5dd9b42afaef7c
Huzaifa <daxing.huzaifa123@gmail.com>
2023-01-19 23:05:25 +0500

    Fix some bugs

--- script.js
+++ script.js
@@ -1,95 +1,97 @@
-// API key
+// Initializing Variables
 
-let apiKey = "94da383a8e0446098e4988026f5168e5"
+ let business = document.getElementById('Business')
+ let sports = document.getElementById('Sports')
+ let entertainment = document.getElementById('Entertainment')
+ let technology = document.getElementById('Technology')
 
-// API'S
-
-const HEADLINES_NEWS = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
-const BUSINESS_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`;
-const SPORTS_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${apiKey}`;
-const ENTERTAINMENT_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=${apiKey}`;
-const TECHNOLOGY_NEWS = `https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=8&apiKey=${apiKey}`;
-const SEARCH_NEWS = `https://newsapi.org/v2/everything?q=`;
-
-let business = document.getElementById('Business')
-let sports = document.getElementById('Sports')
-let entertainment = document.getElementById('Entertainment')
-let technology = document.getElementById('Technology')
-
-let newsLocation = document.getElementById('newsLocation');
-let search = document.getElementById('search');
-let searchInput = document.getElementById('searchInput');
+ let newsLocation = document.getElementById('newsLocation');
+ let search = document.getElementById('search');
+ let searchInput = document.getElementById('searchInput');
 
 
-displayNews(HEADLINES_NEWS, "Headlines")
-search.classList.add('hidden')
+ displayNews("Trending Topic", "Headlines")
+ search.classList.add('hidden')
 
-business.addEventListener('click', () => {
-  newsLocation.innerHTML = "";
-  displayNews(BUSINESS_NEWS, "Business News");
-})
-sports.addEventListener('click', () => {
-  newsLocation.innerHTML = "";
-  displayNews(SPORTS_NEWS, "Sports News");
-})
-entertainment.addEventListener('click', () => {
-  newsLocation.innerHTML = "";
-  displayNews(ENTERTAINMENT_NEWS, "Entertainment News");
-})
-technology.addEventListener('click', () => {
-  newsLocation.innerHTML = "";
-  displayNews(TECHNOLOGY_NEWS, "Technology News");
-})
-searchInput.addEventListener('input', () =>{
-  var val = searchInput.value
-  if(val == ""){
-    search.classList.remove('inline-flex')
-    search.classList.add('hidden')
-  }else{
-    search.classList.add('inline-flex')
-    search.classList.remove('hidden')
-  }
-})
-search.addEventListener('click', () => {
-  newsLocation.innerHTML = "";
-  let searchVal = searchInput.value;
-  displayNews(SEARCH_NEWS + searchVal + "&apiKey=" + `${apiKey}`, "Results are given below");
-  searchInput.value ="";
-})
+ business.addEventListener('click', () => {
+   newsLocation.innerHTML = "";
+   displayNews("Business", "Business News");
+ })
+ sports.addEventListener('click', () => {
+   newsLocation.innerHTML = "";
+   displayNews("Sports", "Sports News");
+ })
+ entertainment.addEventListener('click', () => {
+   newsLocation.innerHTML = "";
+   displayNews("Entertainment", "Entertainment News");
+ })
+ technology.addEventListener('click', () => {
+   newsLocation.innerHTML = "";
+   displayNews("Technology", "Technology News");
+ })
+ searchInput.addEventListener('input', () =>{
+   var val = searchInput.value
+   if(val == ""){
+     search.classList.remove('inline-flex')
+     search.classList.add('hidden')
+   }else{
+     search.classList.add('inline-flex')
+     search.classList.remove('hidden')
+   }
+ })
+ search.addEventListener('click', () => {
+   newsLocation.innerHTML = "";
+   let searchVal = searchInput.value;
+   displayNews(`${searchVal}`, "Results are given below");
+   searchInput.value ="";
+ })
 
-function displayNews(api, text) {
-  document.getElementById("load").style.display = "block";
-  document.getElementById('newsTxt').innerHTML = text
-  let response = fetch(api);
-  response.then((response) => {
+ function displayNews(api, text) {
+   document.getElementById("load").style.display = "block";
+   document.getElementById('newsTxt').innerHTML = text;
+   const options = {
+    method: 'GET',
+    headers: {
+      'X-BingApis-SDK': 'true',
+      'X-RapidAPI-Key': 'c42bd16b3cmsh30dcb16d056fb4fp1eb86ajsn1bf545a568cb',
+      'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+    }
+  };
+ 
+  fetch(`https://bing-news-search1.p.rapidapi.com/news/search?q=${api}&freshness=Day&textFormat=Raw&safeSearch=Off`, options)
+  .then((response) => {
     return response.json();
   }).then((data) => {
     document.getElementById("load").style.display = "none";
-    let articles = data.articles
-    let ihtml = ""
-    for (news in articles) {
-      // console.log(articles[news]);
-      ihtml += `
-
-      <div class="p-4 lg:w-1/3">
-      <div
-        class="h-[90%] bg-gray-200 bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-        <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">${articles[news].title}</h2>
-        <img src="${articles[news].urlToImage}" alt="">
-        <p class="leading-relaxed mb-3">${articles[news].description}</p>
-        <a href = "${articles[news].url}" class="text-indigo-400 flex justify-evenly">
-          Details
-          <span>${articles[news].source.name}</span>
-        </a>
+    let articles = data.value;
+     let ihtml = ""
+     for (news in articles) {
+      try {
+        ihtml += `
+        <div class="p-4 lg:w-1/3">
+        <div
+          class="h-[90%] bg-gray-200 bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">${articles[news].name}</h2>
+          <img src="${articles[news].image.thumbnail.contentUrl}" alt="" class="width">
+          <p class="leading-relaxed mb-3">${articles[news].description}</p>
+          <a href = "${articles[news].url}" class="text-indigo-400 flex justify-evenly">
+            Details
+            <span>${articles[news].datePublished}</span>
+          </a>
+        </div>
       </div>
-    </div>
-            `
-    }
-    let newsLocation = document.getElementById('newsLocation');
-    newsLocation.innerHTML = ihtml
-  }).catch(error => {
-    // Hide the loading icon
-    document.getElementById("load").style.display = "none";
-    console.log(error);
-  });
-}
+              `
+      } catch (error) {
+        console.log(error);
+      }
+       
+     }
+     let newsLocation = document.getElementById('newsLocation');
+     newsLocation.innerHTML = ihtml
+   })
+  //  .catch(error => {
+  //    // Hide the loading icon
+  //    document.getElementById("load").style.display = "none";
+  //    console.log(error);
+  //  });
+ }
