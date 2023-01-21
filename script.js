e90e09265c08f401ee948f9c4442b25a33a8388f
Huzaifa <daxing.huzaifa123@gmail.com>
2023-01-22 04:06:31 +0500

    Add Details Button

--- script.js
+++ script.js
@@ -29,12 +29,12 @@ technology.addEventListener('click', () => {
   newsLocation.innerHTML = "";
   displayNews("Technology", "Technology News");
 })
-searchInput.addEventListener('input', () =>{
+searchInput.addEventListener('input', () => {
   var val = searchInput.value
-  if(val == ""){
+  if (val == "") {
     search.classList.remove('inline-flex')
     search.classList.add('hidden')
-  }else{
+  } else {
     search.classList.add('inline-flex')
     search.classList.remove('hidden')
   }
@@ -43,22 +43,23 @@ search.addEventListener('click', () => {
   newsLocation.innerHTML = "";
   let searchVal = searchInput.value;
   displayNews(`${searchVal}`, "Results are given below");
-  searchInput.value ="";
+  searchInput.value = "";
 })
 
 function displayNews(api, text) {
   document.getElementById("load").style.display = "block";
   document.getElementById('newsTxt').innerHTML = text;
 
- fetch(`https://api.worldnewsapi.com/search-news?text=${api}&api-key=bf9d59dbdbe24e80bdf0faab9065e2d0`)
- .then((response) => {
-   return response.json();
- }).then((data) => {
-   document.getElementById("load").style.display = "none";
-   let articles = data.news;
-   let ihtml = ""
-   for (news in articles) {
-      // console.log(articles[news]);
+  fetch(`https://api.worldnewsapi.com/search-news?text=${api}&api-key=bf9d59dbdbe24e80bdf0faab9065e2d0`)
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      document.getElementById("load").style.display = "none";
+      let articles = data.news;
+      let ihtml = ""
+      for (news in articles) {
+        // console.log(articles[news]);
         try {
           ihtml += `
           <div class="p-4 lg:w-1/3 mobile:p-0">
@@ -68,23 +69,26 @@ function displayNews(api, text) {
             <img src="${articles[news].image}" alt="" class="width">
             <p class="leading-relaxed mb-3">Publish Date: ${articles[news].publish_date}</p>
             <span> Author: ${articles[news].author} </span>
-            <a href = "${articles[news].url}" class="text-indigo-400 flex justify-evenly">
-              Details
-            </a>
+            <button class="learn-more mt-1">
+            <span class="circle" aria-hidden="true">
+            <span class="icon arrow"></span>
+            </span>
+            <span class="button-text"> <a href = "${articles[news].url}" class="text-white"> Learn More </a></span>
+              </button>
           </div>
         </div>
                 `
         } catch (error) {
           console.log(error);
         }
-         
-       }
-       let newsLocation = document.getElementById('newsLocation');
-       newsLocation.innerHTML = ihtml
-     })
-    //  .catch(error => {
-    //    // Hide the loading icon
-    //    document.getElementById("load").style.display = "none";
-    //    console.log(error);
-    //  });
-   }
+
+      }
+      let newsLocation = document.getElementById('newsLocation');
+      newsLocation.innerHTML = ihtml
+    })
+  //  .catch(error => {
+  //    // Hide the loading icon
+  //    document.getElementById("load").style.display = "none";
+  //    console.log(error);
+  //  });
+}
